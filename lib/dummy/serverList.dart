406ccd56a1e22dcdec0b51cada401cce6e027397
James Christian Wira <jamesch888@gmail.com>
2021-08-02 04:28:57 +0700

    Place for dummy data

---
+++ lib/dummy/serverList.dart
@@ -0,0 +1,69 @@
+import 'package:discord_recreation/model/server.dart';
+
+List<Server> dummyServers = [
+  Server(
+    id: 1,
+    serverName: 'Cerdas',
+    thumbnailUrl:
+        'https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg',
+  ),
+  Server(
+    id: 2,
+    serverName: 'Playhard',
+    thumbnailUrl:
+        'https://d2047i8eds3u0g.cloudfront.net/ja/hearthstone/original/1X/e003362e23b2f5d2d1a2a3d22f7c419d7c7a3c5d.png',
+  ),
+  Server(
+    id: 3,
+    serverName: 'Aslab',
+    thumbnailUrl: 'https://freesvg.org/img/1286146771.png',
+  ),
+  Server(
+    id: 4,
+    serverName: 'Kantor',
+    thumbnailUrl:
+        'https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg',
+  ),
+  Server(
+    id: 5,
+    serverName: 'Rumah',
+    thumbnailUrl:
+        'https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg',
+  ),
+  Server(
+    id: 6,
+    serverName: 'Amogus',
+    thumbnailUrl:
+        'https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg',
+  ),
+  Server(
+    id: 7,
+    serverName: 'Jonas',
+    thumbnailUrl:
+        'https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg',
+  ),
+  Server(
+    id: 8,
+    serverName: 'Kyedae',
+    thumbnailUrl:
+        'https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg',
+  ),
+  Server(
+    id: 9,
+    serverName: 'Stardye',
+    thumbnailUrl:
+        'https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg',
+  ),
+  Server(
+    id: 10,
+    serverName: 'Boomi',
+    thumbnailUrl:
+        'https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg',
+  ),
+  Server(
+    id: 11,
+    serverName: 'NSFW',
+    thumbnailUrl:
+        'https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg',
+  ),
+];
