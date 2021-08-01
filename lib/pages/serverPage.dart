4e5cf7cd88940612bdee8fd252e9153f5a30963b
James Christian Wira <jamesch888@gmail.com>
2021-08-02 04:30:15 +0700

    Feat: Adding several unused pages for later

---
+++ lib/pages/serverPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ServerPage extends StatelessWidget {
+  static const routeName = '/server';
+  static const pageTitle = "Server";
+
+  const ServerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
