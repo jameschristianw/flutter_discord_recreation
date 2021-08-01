ee5a44a8b8e7cec44f808216e5137d3b97b04593
James Christian Wira <jamesch888@gmail.com>
2021-08-02 04:29:24 +0700

    Feat: Adding Server and User model

---
+++ lib/model/server.dart
@@ -0,0 +1,11 @@
+class Server {
+  final int? id;
+  final String? serverName;
+  final String? thumbnailUrl;
+
+  Server({
+    this.id,
+    this.serverName,
+    this.thumbnailUrl,
+  });
+}
---
+++ lib/model/user.dart
@@ -0,0 +1,15 @@
+class User {
+  final int? id;
+  final String? username;
+  final String? email;
+  final String? phoneNo;
+  final String? profilePictureUrl;
+
+  User({
+    this.id,
+    this.username,
+    this.email,
+    this.phoneNo,
+    this.profilePictureUrl,
+  });
+}
