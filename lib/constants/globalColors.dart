2bcbd8b37637dff8a01314eba94ff0a1a4d24729
James Christian Wira <jamesch888@gmail.com>
2021-08-02 07:34:34 +0700

    Feat: Adding global variable for colors

---
+++ lib/constants/globalColors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color darkGrey = Color.fromRGBO(32, 34, 37, 1);
+Color darkerGrey = Color.fromRGBO(42, 43, 47, 1);
+Color lightGrey = Color.fromRGBO(48, 49, 54, 1);
+Color lighterGrey = Color.fromRGBO(54, 57, 64, 1);
+
+Color textColor = Color.fromRGBO(141, 144, 149, 1);
+
+Color selectedColor = Color.fromRGBO(57, 60, 67, 1);
