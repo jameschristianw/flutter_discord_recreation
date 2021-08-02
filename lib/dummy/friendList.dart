8e8c080fa8408d55d31c71f79580620730b9bcee
James Christian Wira <jamesch888@gmail.com>
2021-08-02 07:35:24 +0700

    Adding dummy data for friend list

---
+++ lib/dummy/friendList.dart
@@ -0,0 +1,74 @@
+import 'package:discord_recreation/model/user.dart';
+
+List<User> dummyFriendList = [
+  User(
+    id: 1234,
+    email: "email@gmail.com",
+    phoneNo: "+628123456789",
+    username: "username",
+  ),
+  User(
+    id: 1234,
+    email: "email@gmail.com",
+    phoneNo: "+628123456789",
+    username: "username",
+    profilePictureUrl:
+        "https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg",
+  ),
+  User(
+    id: 1234,
+    email: "email@gmail.com",
+    phoneNo: "+628123456789",
+    username: "username",
+    profilePictureUrl:
+        "https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg",
+  ),
+  User(
+    id: 1234,
+    email: "email@gmail.com",
+    phoneNo: "+628123456789",
+    username: "username",
+    profilePictureUrl:
+        "https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg",
+  ),
+  User(
+    id: 1234,
+    email: "email@gmail.com",
+    phoneNo: "+628123456789",
+    username: "username",
+    profilePictureUrl:
+        "https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg",
+  ),
+  User(
+    id: 1234,
+    email: "email@gmail.com",
+    phoneNo: "+628123456789",
+    username: "username",
+    profilePictureUrl:
+        "https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg",
+  ),
+  User(
+    id: 1234,
+    email: "email@gmail.com",
+    phoneNo: "+628123456789",
+    username: "username",
+    profilePictureUrl:
+        "https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg",
+  ),
+  User(
+    id: 1234,
+    email: "email@gmail.com",
+    phoneNo: "+628123456789",
+    username: "username",
+    profilePictureUrl:
+        "https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg",
+  ),
+  User(
+    id: 1234,
+    email: "email@gmail.com",
+    phoneNo: "+628123456789",
+    username: "username",
+    profilePictureUrl:
+        "https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg",
+  ),
+];
