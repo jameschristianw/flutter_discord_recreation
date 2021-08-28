96340e4ad0de9c92a4ba3647b303f9eff57961af
James Christian Wira <jamesch888@gmail.com>
2021-08-28 22:35:27 +0700

    Feat: Adding Friends Page Header constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,17 @@
+enum Menu { Friends, StageDiscovery, Nitro }
+
+enum FriendsHeader { Online, All, Pending, Blocked }
+// extension MenuExtension on Menu {
+//   int get menuValue {
+//     switch (this) {
+//       case Menu.Friends:
+//         return 0;
+//       case Menu.StageDiscovery:
+//         return 1;
+//       case Menu.Nitro:
+//         return 2;
+//       default:
+//         return -1;
+//     }
+//   }
+// }
