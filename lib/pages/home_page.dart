c6945b816b761bc9425e8f7567e404a31550c284
James Christian Wira <jamesch888@gmail.com>
2021-08-28 22:36:45 +0700

    Fix: Adding navigation for each home menu

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,39 @@
-import 'package:discord_recreation/components/quickChatSection/quick_chat.dart';
-import 'package:discord_recreation/components/test.dart';
+import 'package:discord_recreation/components/home_components/friend_section/friend_list.dart';
+import 'package:discord_recreation/components/home_components/nitro_section/nitro_detail.dart';
+import 'package:discord_recreation/components/home_components/quick_chat_section/quick_chat.dart';
+import 'package:discord_recreation/components/home_components/stage_discovery_section/stage_discovery_detail.dart';
+import 'package:discord_recreation/components/left_navigation_bar/navigation_bar.dart';
+import 'package:discord_recreation/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
-
-import 'package:discord_recreation/components/leftNavigationBar/navigation_bar.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   static const routeName = '/';
   static const pageTitle = "Discord";
 
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  final List<Widget> subMenu = [
+    FriendList(),
+    StageDiscoveryDetail(),
+    NitroDetail(),
+  ];
 
   @override
   Widget build(BuildContext context) {
+    MenuProvider provider = Provider.of<MenuProvider>(context);
+
     return Scaffold(
       body: Container(
         child: Row(
           children: [
             NavigationBar(),
             QuickChat(),
+            // Expanded(
+            //   child: TestWidget(),
+            // ),
             Expanded(
-              child: TestWidget(),
+              child: subMenu[provider.currentMenu],
             ),
           ],
         ),
